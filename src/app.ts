5fd261ac9ae7e8bfef6a6c772bddca721dc007e2
Yishnu Pramanik <yishnupramanik96@gmail.com>
2021-01-31 20:28:35 +0530

    .

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
 
+import express from 'express';
+import bodyParser from 'body-parser';
+import mailgun from 'mailgun-js';
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-const mailgun = require("mailgun-js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
